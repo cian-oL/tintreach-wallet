acdc0c4ad997dd3e69f31f85d96c7c1dda01d4e2
cian-oL <cianoleary2010@gmail.com>
2024-11-21 12:56:09 +0000

    Refactored server to remove unnecessary pool and add security protection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,27 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
-import pool from "./db";
 import userRoute from "./routes/userRoute";
-
-pool
-  .connect()
-  .then(() => console.log("Connected to database"))
-  .catch((err) => console.log("failed to connect to database", err));
+import authRoute from "./routes/authRoute";
 
 const PORT = (process.env.PORT as string) || "8080";
 
 const app = express();
 app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(express.json());
 app.use(
   express.urlencoded({
